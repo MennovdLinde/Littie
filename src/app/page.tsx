e319e70ff117abe748b2ce0d9dfdb331d95fd5a3
MennovdLinde <mennovandelinde@gmail.com>
2025-01-17 14:54:00 +0100

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
         <nav className="flex space-x-4 justify-between sm:items-center sm:pr-0 pr-4">
           <button
             onClick={() => handleNavClick("home")}
-            className={`text-[#076447] hover:text-[#F6F3EE] px-5 sm:px-1 ${
+            className={`text-[#076447] hover:text-[#F6F3EE] sm:px-5 px-1   ${
               currentSection === "home" ? "border-b-2 border-[#076447]" : ""
             }`}
           >
@@ -156,7 +156,7 @@ export default function Home() {
           </button>
           <button
             onClick={() => handleNavClick("portofolio")}
-            className={`text-[#076447] hover:text-[#F6F3EE] px-5 sm:px-1 ${
+            className={`text-[#076447] hover:text-[#F6F3EE] sm:px-5 px-1 ${
               currentSection === "portofolio"
                 ? "border-b-2 border-[#076447]"
                 : ""
@@ -166,7 +166,7 @@ export default function Home() {
           </button>
           <button
             onClick={() => handleNavClick("contact")}
-            className={`text-[#076447] hover:text-[#F6F3EE] px-5 sm:px-1 ${
+            className={`text-[#076447] hover:text-[#F6F3EE] sm:px-5 px-1 ${
               currentSection === "contact" ? "border-b-2 border-[#076447]" : ""
             }`}
           >
